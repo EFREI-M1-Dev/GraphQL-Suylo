3313915ce61d6062cd6d94aa76231befe62ebab4
Suylo <spoownz@yahoo.com>
2024-02-20 10:39:30 +0100

    add gql files

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,36 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { useQuery } from '@urql/vue'
+
+import { graphql } from './gql'
+import {computed} from "vue";
+
+const { data } = useQuery({
+  query: graphql(/* GraphQL */ `
+    query {
+        characters(page: 1) {
+        info {
+          count
+        }
+        results {
+          name
+        }
+      }
+    }
+  `),
+  variables: { first: 10 }
+})
+
+const films = data;
 </script>
 
 <template>
   <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+    <h1>Rick and morty</h1>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <div class="list">
+    {{ films }}
+  </div>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style scoped lang="scss">
 </style>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,14 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { createClient, defaultExchanges, dedupExchange, fetchExchange, provideClient } from '@urql/vue';
 
-createApp(App).mount('#app')
+const client = createClient({
+    url: 'https://rickandmortyapi.com/graphql',
+    exchanges: [dedupExchange, fetchExchange],
+});
+
+const app = createApp(App);
+
+app.use(provideClient(client));
+app.mount('#app')
